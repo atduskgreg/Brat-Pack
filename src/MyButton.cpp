4ae61063a8a2ff40c1673ba3059d0693eac5ff84
Rune Skjoldborg Madsen <rune@runemadsen.com>
2011-03-15 12:13:31 -0400

    working on getting ruby script to run

--- src/MyButton.cpp
+++ src/MyButton.cpp
@@ -28,7 +28,27 @@ void MyButton::onPress(int x, int y, int button)
 	s.canChooseFiles = false;
 	s.allowMultipleSelection = false;
 	files = openDialog(s);
-	string command = ofToDataPath("ruby/new_sinatra_app.rb", true) + " " + files[0];
-	system(command.c_str());
+	string command = ofToDataPath("ruby/new_sinatra_app.rb", true);
+	vector <string> split = ofSplitString(command, " ");
+	cout << split.size();
+	string result = "";
+	
+	for(int i = 0; i < split.size(); i++)
+	{
+		result += split[i];
+		
+		if(i < split.size() - 1)
+		{
+			result += "\\ ";
+		}
+	}
+	
+	result += " " + command + " " + files[0];
+	
+	
+	cout << result << endl;
+	system(result.c_str());
+	
+	cout << system("ruby -v") << endl;
 }
 
