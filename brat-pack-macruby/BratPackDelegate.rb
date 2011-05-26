1d18ca65d3a728d4ca21d15196382bcb2aab0f95
Greg Borenstein <greg@mfdz.com>
2011-05-26 15:31:08 -0400

    added iframe for documentation browsing

--- brat-pack-macruby/BratPackDelegate.rb
+++ brat-pack-macruby/BratPackDelegate.rb
@@ -20,9 +20,7 @@ class BratPackDelegate
 
 
     @project_manager = ProjectManager.new
-    #@project_manager.name = "joe"
     @project_manager.projects = Project.create_many_projects
-     #@projects = Project.create_many_projects   
   end
   
   def webView(view, didFinishLoadForFrame:frame)
@@ -32,43 +30,14 @@ class BratPackDelegate
     @project_manager.respondsToSelector("stop:")
 
     @project_manager.respondsToSelector("projects")
-    #@project_manager.respondsToSelector("name")
-
-    #@js_engine.setValue(@projects, forKey: "projects")
-    
-    # JIT the method, no colon at the end of the method since the selector doesn't
-    # take arguments.
-    
-    #puts @project_manager.start("foo")
-    
-
     
     @project_manager.projects.each_with_index do |project,i|
       project.respondsToSelector("name")
       project.respondsToSelector("running")
       project.respondsToSelector("id")
-      
-     #@js_engine.evaluateWebScript("addProject(ProjectManager.projects()[#{i}])")
     end
     
-    
-    
     @js_engine.evaluateWebScript('displayProjects()')
-
-    
-    #puts @project_manager.name()
-    #puts @js_engine.evaluateWebScript("projectManager.start_('cats')")
-  
-   #puts @js_engine.evaluateWebScript("projectManager.projects()[0].start()")
-    #puts "js bridge test: "
-    # trigger JS from Ruby
-    #@js_engine.evaluateWebScript("$('body').text('phase 1');")
-    # Evaluate JS from Ruby via the DOM
-    #puts @js_engine.evaluateWebScript("projectManager.projects()")
-    # Execute Ruby code from the DOM
-    #@project.start
-
-    #puts @js_engine.evaluateWebScript("project.start()")
   end
 
 end
